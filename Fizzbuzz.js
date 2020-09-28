5314be881f3b659f1b7613b7c410d100565687e6
Rachel <thelivedish@gmail.com>
2020-09-27 21:52:09 -0700

    fizzbuzz accepted

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,19 +1,26 @@
 function Fizzbuzz1(a, b){
-    let str ="";
-    for(let i = 0; i<= 100; i++){
-        if(i % a === 0 && i % 5 ===0){
+    let str =" ";
+    for(let i = 0; i <= 100; i++){
+        if(i % a == 0 && i % b ==0){
             str += "Fizz Buzz"
             console.log(str)
         }
-        else if (i % a ===0){
-            str += " Fizz ";
+        else if (i % a ==0){
+            str += "Fizz ";
         }
-        else if(i % b ===0){
-            str += " Buzz "
+        else if(i % b ==0){
+            str += "Buzz "
         }
         else{
             str += i + " ";
         }
-    }
-    return str;
+    } return str;
+   
+}
+function buzzIt(){
+  let output = "";
+  let value1 = document.getElementById('Fizzvalue').value;
+  let value2 = document.getElementById('Buzzvalue').value;
+    output = Fizzbuzz1(value1, value2);
+    document.getElementById('results').innerHTML = output;
 }
